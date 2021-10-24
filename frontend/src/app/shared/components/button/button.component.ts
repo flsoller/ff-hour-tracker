b22c09f7fda6a500b2e40b27c5ea356422562a2b
Florian Soller <flsoller@gmail.com>
2021-10-24 23:06:05 +0200

    Add button style

--- frontend/src/app/shared/components/button/button.component.ts
+++ frontend/src/app/shared/components/button/button.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent {
+  @Input() label = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  @Input() neoButton = false;
 }
