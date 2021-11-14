98692d0f9ae1e3db3868ad66b3ed88971de56850
Florian Soller <flsoller@gmail.com>
2021-11-14 16:50:57 +0100

    Add sidebar menu items.

--- frontend/src/app/navigation/sidebar/sidebar.component.ts
+++ frontend/src/app/navigation/sidebar/sidebar.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import {
+  faUsers,
+  faBox,
+  faTools,
+  faUserClock,
+  faFileInvoice,
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent implements OnInit {
-  faUser = faUser;
+  faUsers = faUsers;
+  faBox = faBox;
+  faTools = faTools;
+  faUserClock = faUserClock;
+  faFileInvoice = faFileInvoice;
 
   constructor() {}
 
