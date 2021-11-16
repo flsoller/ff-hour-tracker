cefe5225622214bedc1ff1354028e402e995d4c3
Florian Soller <flsoller@gmail.com>
2021-11-16 20:24:08 +0100

    Add empty app modules

---
+++ frontend/src/app/time-logging/time-logging.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TimeLoggingRoutingModule } from './time-logging-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TimeLoggingRoutingModule
+  ]
+})
+export class TimeLoggingModule { }
