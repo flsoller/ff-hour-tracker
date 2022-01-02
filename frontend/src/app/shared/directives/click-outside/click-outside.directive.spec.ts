e99952ec7683ab17c42bfd1d26fe4b8be5bd51b7
Florian Soller <flsoller@gmail.com>
2022-01-02 15:16:11 +0100

    Add tests for directive.

--- frontend/src/app/shared/directives/click-outside/click-outside.directive.spec.ts
+++ frontend/src/app/shared/directives/click-outside/click-outside.directive.spec.ts
@@ -1,8 +1,51 @@
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ClickOutsideDirective } from './click-outside.directive';
 
+@Component({
+  selector: 'click-outside-test-comp',
+  template: `
+    <div class="outside">
+      <div
+        class="inside"
+        appClickOutside
+        (outsideElClick)="toggleClick($event)"></div>
+    </div>
+  `,
+})
+class TestComponent {
+  clicked = false;
+  toggleClick() {
+    this.clicked = !this.clicked;
+  }
+}
+
 describe('ClickOutsideDirective', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let insideEl: DebugElement;
+  let outsideEl: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [TestComponent, ClickOutsideDirective],
+    });
+
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    insideEl = fixture.debugElement.query(By.css('.inside'));
+    outsideEl = fixture.debugElement.query(By.css('.outside'));
+  });
+
   it('should create an instance', () => {
-    const directive = new ClickOutsideDirective();
+    const directive = new ClickOutsideDirective(insideEl);
     expect(directive).toBeTruthy();
+  });
+
+  it('should emit when clicking outside the element', () => {
+    outsideEl.nativeElement.click();
+    expect(component.clicked).toBe(true);
   });
 });
