eb455d283a61f5cd95f399bc577f725d7a005022
Florian Soller <flsoller@gmail.com>
2022-01-15 19:24:04 +0100

    Initial setup for vue conversion

---
+++ frontend-web/src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Hello World</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: '',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style scoped lang="scss"></style>
---
+++ frontend-web/vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  devServer: {
+    disableHostCheck: true,
+    port: 4200,
+    public: '0.0.0.0:4200',
+  },
+};
