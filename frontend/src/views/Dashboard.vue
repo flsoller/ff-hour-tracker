27e69b769f2bf41816c1a0b3aef5816cc69af4b7
radum2o18 <radu.lucian.milos@gmail.com>
2022-05-14 21:24:06 +0200

    Add native ui package

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,12 +1,19 @@
 <template>
   <h1>Dashboard</h1>
+
+ <n-button type="primary">
+      Primary
+  </n-button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+ import { NButton } from 'naive-ui'
 
 export default defineComponent({
   name: 'Dashboard',
-  components: {},
+  components: {
+    NButton
+  },
 });
 </script>
