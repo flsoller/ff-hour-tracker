401ae9f436d164dfdc25eebf4e046bac28c27c57
Florian Soller <flsoller@gmail.com>
2023-06-30 20:12:59 +0200

    feat(infra): refactor stacks into constructs
    - compose stack with constructs as per docs best practices

---
+++ aws_cdk/lib/index.ts
@@ -0,0 +1,29 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { AuthorizerService } from './constructs/api_authorizer';
+import { HourTrackerApiGateway } from './constructs/api_gateway';
+import { UsersService } from './constructs/users';
+import { HOUR_TRACKER } from './constants/stacks';
+
+export class HourTrackerApi extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const defaultAuthorizer = new AuthorizerService(
+      this,
+      HOUR_TRACKER.API_AUTHORIZER
+    );
+
+    const apiGateway = new HourTrackerApiGateway(
+      this,
+      HOUR_TRACKER.API_GATEWAY,
+      {
+        authService: defaultAuthorizer.authorizer,
+      }
+    );
+
+    new UsersService(this, HOUR_TRACKER.USERS_API, {
+      apiGateway: apiGateway.httpApiGateway,
+    });
+  }
+}
