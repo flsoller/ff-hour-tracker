45fcc819f811524c162acb88807c97c63e425b6a
Florian Soller <flsoller@gmail.com>
2024-10-13 23:32:10 +0200

    feat(organization-manager): add manage-organization logic

--- infrastructure/lib/org-manager/index.ts
+++ infrastructure/lib/org-manager/index.ts
@@ -2,6 +2,7 @@ import * as cdk from "aws-cdk-lib";
 import * as ecr from "aws-cdk-lib/aws-ecr";
 import * as ssm from "aws-cdk-lib/aws-ssm";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import { Duration } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { RemovalPolicy } from "aws-cdk-lib";
 import { HOUR_TRACKER_ECR_REPO_NAMES } from "../constants/ecr";
@@ -15,6 +16,14 @@ export class HourTrackerOrganizationManager extends cdk.Stack {
     const dbConnectionString = ssm.StringParameter.valueForStringParameter(
       this,
       ENVIRONMENT_PARAMS.DATABASE_URL_KEY
+    );
+    const supportUserEmail = ssm.StringParameter.valueForStringParameter(
+      this,
+      ENVIRONMENT_PARAMS.SUPPORT_USER_EMAIL
+    );
+    const supportUserPassword = ssm.StringParameter.valueForStringParameter(
+      this,
+      ENVIRONMENT_PARAMS.SUPPORT_USER_PW
     );
 
     const hashOrVersion = new cdk.CfnParameter(this, "hashOrVersion", {
@@ -38,7 +47,10 @@ export class HourTrackerOrganizationManager extends cdk.Stack {
         architecture: lambda.Architecture.ARM_64,
         environment: {
           DATABASE_URL: dbConnectionString,
+          SUPPORT_USER_EMAIL: supportUserEmail,
+          SUPPORT_USER_PW: supportUserPassword,
         },
+        timeout: Duration.seconds(15),
       }
     );
     organizationManager.applyRemovalPolicy(RemovalPolicy.DESTROY);
