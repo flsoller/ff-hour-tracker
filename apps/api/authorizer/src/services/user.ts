8bb34fe99d3bc28a2d3453524e105612f34b1365
Florian Soller <flsoller@gmail.com>
2024-11-12 21:06:16 +0100

    feat(api): implement authorizer logic

---
+++ apps/api/authorizer/src/services/user.ts
@@ -0,0 +1,34 @@
+import { db, models, DrizzleORM } from "@hour-tracker/db";
+
+export async function getActiveUser(userId: string, organizationId: string) {
+  const [user] = await db
+    .select({
+      id: models.users.id,
+      organizationId: models.users.organizationId,
+      role: models.users.role,
+      active: models.users.active,
+    })
+    .from(models.users)
+    .where(
+      DrizzleORM.and(
+        DrizzleORM.eq(models.users.id, userId),
+        DrizzleORM.eq(models.users.organizationId, organizationId)
+      )
+    )
+    .limit(1);
+
+  if (!user) {
+    return {
+      isActive: false,
+      user: null,
+    };
+  }
+
+  const { active, ...userProps } = user;
+  return {
+    isActive: active,
+    user: {
+      ...userProps,
+    },
+  };
+}
